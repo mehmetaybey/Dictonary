14b7f842021c61ae8071458466da32b8e4e44c97
Mehmet AYBEY <mehmetaybey@aof.anadolu.edu.tr>
2021-01-22 14:41:59 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Dictonary
+{
+    class MyDictionary<Y,N>
+    {
+        Y[] _yemek;
+        N[] _numara;
+
+        public MyDictionary()
+        {
+            _yemek = new Y[0];
+            _numara = new N[0];
+        }
+
+        public void Add(Y yemek, N numara)
+        {
+            Y[] _yemekAc = _yemek;
+            N[] _numaraSira = _numara;
+
+            _yemek = new Y[_yemek.Length + 1];
+            _numara = new N[_numara.Length + 1];
+            for (int i = 0; i < _yemekAc.Length; i++)
+            {
+                _yemek[i] = _yemekAc[i];
+                _numara[i] = _numaraSira[i];
+            }
+
+            _yemek[_yemek.Length - 1] = yemek;
+            _numara[_numara.Length - 1] = numara;
+
+        }
+
+        public Y[] Yemek
+        {
+            get { return _yemek; }
+        }
+
+        public N[] Numara
+        {
+            get { return _numara; }
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> yemek = new MyDictionary<string, int>();
+            yemek.Add("Adana",1);
+            yemek.Add("pide",2);
+            yemek.Add("Mantı",3);
+            yemek.Add("Menemen",4);
+            yemek.Add("İçli Köfte",5);
+            yemek.Add("Beyti",6);
+            yemek.Add("Peynir Helvası",7);
+            yemek.Add("Cağ Kebabı",8);
+
+            foreach (var yemekler in yemek.Yemek)
+            {
+                Console.WriteLine("Yemeğin adı :" +yemekler );
+            }
+
+            foreach (var sirasi in yemek.Numara)
+            {
+                
+            }
+        }
+    }
+}
